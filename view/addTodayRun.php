0b1b3e99c6ce79914066d7b938e7bff830a7fc05
rekky1aws <killikomfr@gmail.com>
2023-08-30 17:04:28 +0200

    Initialized today adder
    Created view folder
    Created controller/add
    Initialized files
    HTML code in view/addTodayRun.php to display buttons

---
+++ view/addTodayRun.php
@@ -0,0 +1,7 @@
+<div class="today_container">
+	<span class="today_title"> Today 's run was :</span>
+	<div>
+		<a class="today_button success" href="">Successful</a>
+		<a class="today_button fail" href="">Failed</a>
+	</div>
+</div>
