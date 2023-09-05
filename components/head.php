7c2ecd3e0fb80b2bf277c56d265acce02c2f9bf4
rekky1aws <killikomfr@gmail.com>
2023-09-05 20:19:19 +0100

    Display style init

--- components/head.php
+++ components/head.php
@@ -7,6 +7,7 @@ class Head
 	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 	<link rel=\"stylesheet\" type=\"text/css\" href=\"".PATH_PREFIX."/style/main.css\">
 	<link rel=\"stylesheet\" type=\"text/css\" href=\"".PATH_PREFIX."/style/addTodayRun.css\">
+	<link rel=\"stylesheet\" type=\"text/css\" href=\"".PATH_PREFIX."/style/displays.css\">
 	";
 
 	public function getContent ()
